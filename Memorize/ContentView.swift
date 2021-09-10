caf17bd524240e0acdc17df259957977aeea0c7e
Abdug'affor Abdurahimov <abdugaffor.abdurahimov@inoviagroup.se>
2021-09-10 18:04:15 +0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Abdug'affor on 10/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
