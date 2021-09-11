3a38eaaaea2bfadd9ff0d578848260a98e2351e3
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-09-11 10:17:14 +0500

    Updated

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,38 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis  = ["🥏", "🪀", "🥊", "🎿", "🏓"]
+    
     var body: some View {
         
+
         HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+            ForEach(emojis, id: \.self ,content: { emoji in
+                CardView(content: emoji, isFaceUp: true)
+            })
         }
         .padding(.horizontal)
         .foregroundColor(.red)
-        
-        
-        
-        
     }
 }
+
+
+struct CardView: View {
+    
+    var content: String = "🏀"
+    @State var isFaceUp: Bool = true
+       
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-
-
-struct CardView: View {
-    var body: some View {
-        ZStack(content: {
-            Text("🏀")
-            
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                .stroke(lineWidth: 3)
-        })
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
